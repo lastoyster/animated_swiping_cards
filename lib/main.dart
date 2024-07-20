f38cc529d4804522e3d84d2638f4ee88f0b09a29
lastoyster <kadampratiksha56@gmail.com>
2024-07-20 13:11:14 +0530

    refactored code

--- lib/main.dart
+++ lib/main.dart
@@ -7,147 +7,212 @@ void main() {
   runApp(const MainApp());
 }
 
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: const SwipeCard(),
+    );
+  }
+}
+
 class SwipeCard extends StatefulWidget {
   const SwipeCard({super.key});
 
   @override
-  _SwipeCardState createState()=> _SwipeCardState();
+  _SwipeCardState createState() => _SwipeCardState();
 }
 
-class _SwipeCardState extends State<SwipeCard>{
-  List<SwipeItem> _swipeItems= <SwipeItem>[];
+class _SwipeCardState extends State<SwipeCard> {
+  final List<SwipeItem> _swipeItems = [];
   MatchEngine? _matchEngine;
-  GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
-  List<String> _names = [ 'Red','Blue','Green','Orange',];
-  List <Color> _TextUtils=[
-    TextUtils.red,
-    TextUtils.blue,
-    TextUtils.green,
-    TextUtils.yellow,
-    TextUtils.orange,
+  final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  final List<String> _names = ['Red', 'Blue', 'Green', 'Orange'];
+  final List<Color> _colors = [
+    Colors.red,
+    Colors.blue,
+    Colors.green,
+    Colors.orange,
   ];
 
   @override
-  void initState(){
-    for(int i=0; i< _names.length ;i++){
-      _swipeItems.add(SwipeItem(
-        content:Content(text:_names[i],color:_TextUtils[i]),
-        likeAction:(){
-          ScaffoldMessanger.of(context).showSnackBar(SnackBar(
-            content:Text('Nope  ${_names[i]}'),
-          ));
-        }
-
-        nopeAction:(){
-        ScaffoldMessanger.of(context).showSnackBar(SnackBar(
-          content:Text('Nope ${_names[i]}'),
-          duration:Duration(milliseconds: 500),
-        ));
-        },
-      superlikeAction:(){
-        ScaffoldMessanger.of(context).showSnackBar(SnackBar(
-          content:Text('Superlikes ${_names[i]}',),
-          duration:Duration(milliseconds: 500),
-        ));
-      }));
-}
-
-    _matchEngine =MatchEngine(swipeItems: _swipeItems);
+  void initState() {
     super.initState();
+    for (int i = 0; i < _names.length; i++) {
+      _swipeItems.add(SwipeItem(
+        content: Content(text: _names[i], color: _colors[i]),
+        likeAction: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Liked ${_names[i]}'),
+          ));
+        },
+        nopeAction: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Nope ${_names[i]}'),
+            duration: const Duration(milliseconds: 500),
+          ));
+        },
+        superlikeAction: () {
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text('Superliked ${_names[i]}'),
+            duration: const Duration(milliseconds: 500),
+          ));
+        },
+      ));
+    }
+
+    _matchEngine = MatchEngine(swipeItems: _swipeItems);
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
-      key:_scaffoldKey,
-      appBar:AppBar(
-        title:'Animated Swipe Cards',
-        left:(){
-          setState((){
+      key: _scaffoldKey,
+      appBar: AppBar(
+        title: const Text('Animated Swipe Cards'),
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back),
+          onPressed: () {
             Get.back();
-          });
-        },
-    right:(){
-      setState((){
-        String code='';
-        Navigator.push(context,MaterialPageRoute(
-          builder:(context)=>CodeViewScreen('Swipe Cards',)));
-      });
-    }
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.code),
+            onPressed: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const CodeViewScreen('Swipe Cards'),
+                ),
+              );
+            },
+          ),
+        ],
       ),
-
- backgroundColo:Get.isDarkMode? TextUtils.darkColor: TextUtils.primaryColor,
- body:Container(
-  child:Column(
-    Container(
-      height:550,
-      child:SwipeCards(
-        matchEngine:_matchEngine!,
-        itemBuilder:(BuildContext context,int index){
-          return Container(
-            alignment:Alignment.center,
-            color:_swipeItems[index].content.color,
-            child:Text(
-              _swipeItems[index].content.text,
-              style:TextStyle(fontSize:size100,color:TextUtils.white),
+      backgroundColor:
+          Get.isDarkMode ? Colors.black : Colors.white,
+      body: Column(
+        children: [
+          Container(
+            height: 550,
+            child: SwipeCards(
+              matchEngine: _matchEngine!,
+              itemBuilder: (BuildContext context, int index) {
+                return Container(
+                  alignment: Alignment.center,
+                  color: _swipeItems[index].content.color,
+                  child: Text(
+                    _swipeItems[index].content.text!,
+                    style: const TextStyle(fontSize: 100, color: Colors.white),
+                  ),
+                );
+              },
+              onStackFinished: () {
+                ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                  content: Text('Finished'),
+                  duration: Duration(milliseconds: 500),
+                ));
+              },
             ),
-          );
-        },
-      onStackFinished:(){
-        ScaffoldMessanger.of(context).showSnackBar(SnackBar(
-          content:Text('Finished'),
-          duration:Duration(milliseconds: 500),
-        ));
-      },
-    ),
-  ),
-  Row(
-    mainAxisAlignment:MainAxisAlignment.spaceEvenly,
-    children:[
-      MaterialButton(
-        onPressed:(){
-      _matchEngine!.currentItem!.nope();
-        },
-        child:Text( 
-          'Nope',
-          style:TextStyle(color:TextUtls.white),
-        ),
-      elevation:5,
-      color:TextUtils.green,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              MaterialButton(
+                onPressed: () {
+                  _matchEngine!.currentItem!.nope();
+                },
+                child: const Text(
+                  'Nope',
+                  style: TextStyle(color: Colors.white),
+                ),
+                elevation: 5,
+                color: Colors.red,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  _matchEngine!.currentItem!.superLike();
+                },
+                child: const Text(
+                  'Superlike',
+                  style: TextStyle(color: Colors.black),
+                ),
+                elevation: 5,
+                color: Colors.blue,
+              ),
+              MaterialButton(
+                onPressed: () {
+                  _matchEngine!.currentItem!.like();
+                },
+                child: const Text(
+                  'Like',
+                  style: TextStyle(color: Colors.white),
+                ),
+                elevation: 5,
+                color: Colors.green,
+              ),
+            ],
+          ),
+        ],
       ),
-    MaterialButton(
-      onPressed:(){
-        _matchEngine!.currentItem!.superLike();
-      },
-      child:Text(
-        'Super;ike',
-        style:TextStyle(color:TextUtils.black),
-        ),
-     elevation:5,
-      color:TextUtils.white,
-      ),
-     MaterialButton(
-      onPressed:(){
-        _matchEngine!.currentItem!.like();
-      },
-      child:Text(
-        'Like',
-        style:TextStyle(color:TextUtils.white),
-        ),
-     elevation:5,
-      color:TextUtils.orange,
-      )
-    ],
-  )
-  ),
-    ),
     );
   }
 }
- 
- class Content{
-  final String? text;
-  final Color? color;
 
-  Content({this.text,this.color});
- }
+class SwipeItem {
+  final Content content;
+  final VoidCallback likeAction;
+  final VoidCallback nopeAction;
+  final VoidCallback superlikeAction;
+
+  SwipeItem({
+    required this.content,
+    required this.likeAction,
+    required this.nopeAction,
+    required this.superlikeAction,
+  });
+}
+
+class MatchEngine {
+  final List<SwipeItem> swipeItems;
+  SwipeItem? currentItem;
+
+  MatchEngine({required this.swipeItems}) {
+    currentItem = swipeItems.isNotEmpty ? swipeItems[0] : null;
+  }
+
+  void cycleItems() {
+    if (swipeItems.isNotEmpty) {
+      swipeItems.add(swipeItems.removeAt(0));
+      currentItem = swipeItems[0];
+    }
+  }
+}
+
+class Content {
+  final String text;
+  final Color color;
+
+  Content({required this.text, required this.color});
+}
+
+class CodeViewScreen extends StatelessWidget {
+  final String title;
+
+  const CodeViewScreen(this.title, {super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: const Center(
+        child: Text('Code view screen'),
+      ),
+    );
+  }
+}
